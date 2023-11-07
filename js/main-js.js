caffa1f279e3ebf7a2e4e7a435414fc04bb7f9c1
enricoforesta <enrico.foresta97@gmail.com>
2023-11-07 18:17:25 +0100

    feat:biglietto

--- js/main-js.js
+++ js/main-js.js
@@ -32,6 +32,13 @@ const elementInput = document.querySelector(".xx-button");
 
 console.log(elementInput);
 
+
+
+
+
+
+
+
 /* 3. Calcolare il prezzo, kilometri inseriti * 0,21€.
     I prezzi finali dovranno essere con massimo due decimali, ES. 10,99€
 
@@ -50,19 +57,44 @@ elementInput.addEventListener(
 
             let elementSomma = (elementKm.value * elementPrezzo);
 
+            // nome e cognome
+            const elementName = document.querySelector(".xx-name");
+
+            console.log(elementName.value);
+
+            const elementNameOut = document.querySelector(".xx-name-out");
+
+            elementNameOut.innerHTML = elementName.value
+
+            // biglietto
+
+            const elementBigliettoOut = document.querySelector(".xx-biglietto-out");
+
+            elementBigliettoOut.innerHTML = "Biglietto Standard";
+
+
+            //condizione
+
             if (elementEta.value < 18) {
 
                 elementSomma -= ((elementSomma / 100) * elementScontoMinorenne);
 
+               // biglietto
+                elementBigliettoOut.innerHTML = "Biglietto Junior";
             }
 
             else if (elementEta.value > 64) {
 
                 elementSomma -= ((elementSomma / 100) * elementScontoOver65);
 
+               // biglietto
+                elementBigliettoOut.innerHTML = "Biglietto Senior";
             }
 
             console.log("Prezzo Finale", elementSomma.toFixed(2), "€");
+
+
+
 
         }
 
